c8acd11963c2b601004a9fe14105c4152c883784
Erick Ruiz <erickuriel238@gmail.com>
2024-05-02 22:20:58 -0600

    Update Navbar component styling for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,15 +23,15 @@ const Navbar = () => {
             <div onClick={handleNav} className='block md:hidden mr-2 p-5 hover:text-white'>
                 {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
 
-            <div className={nav ? 'z-10 fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500'
+            <div className={nav ? 'z-10 fixed h-full left-0 top-0 w-[70%] bg-[#202121] ease-in-out duration-500'
              : 'fixed left-[-100%]'}>
                 <h1 className='text-3xl font-bold primary-color m-4 pl-5'>E. Ruiz</h1>
                 <ul className='p-8 text-2xl'>
-                    <li className='p-2 font-medium flex'><AiFillHome size={30} className='mr-5'/><a href='#about'>About</a></li>
-                    <li className='p-2 font-medium flex'><AiFillInfoCircle size={30} className='mr-5'/><a href='#work'>Work</a></li>
-                    <li className='p-2 font-medium flex'><AiFillMail size={30} className='mr-5'/><a href='#contact'>Contact</a></li>
-                    <li className='p-2 font-medium flex'><AiOutlineGithub size={30} className='mr-5'/><a href='https://github.com/ErickRz21'>GitHub</a></li>
-                    <li className='p-2 font-medium flex'><AiFillInstagram size={30} className='mr-5'/><a href='https://www.instagram.com/ericks_ruiz/'>Instagram</a></li>
+                    <li className='p-2 font-medium flex'><AiFillHome size={30} className='mr-3'/><a href='#about'>About</a></li>
+                    <li className='p-2 font-medium flex'><AiFillInfoCircle size={30} className='mr-3'/><a href='#work'>Work</a></li>
+                    <li className='p-2 font-medium flex'><AiFillMail size={30} className='mr-3'/><a href='#contact'>Contact</a></li>
+                    <li className='p-2 font-medium flex'><AiOutlineGithub size={30} className='mr-3'/><a href='https://github.com/ErickRz21'>GitHub</a></li>
+                    <li className='p-2 font-medium flex'><AiFillInstagram size={30} className='mr-3'/><a href='https://www.instagram.com/ericks_ruiz/'>Instagram</a></li>
                 </ul>
             </div>
             </div>
